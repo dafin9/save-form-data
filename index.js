5e8ed4ca41c58162f3d878805b4dcca04d5f9917
dafin9 <abhi.d3v3lop3r@gmail.com>
2024-02-09 00:09:21 +0530

    fix

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+import express, { urlencoded } from 'express';
 const app = express();
 const port = 3000;
 
 // Middleware to parse form data
-app.use(express.urlencoded({ extended: true }));
+app.use(urlencoded({ extended: true }));
 
 app.get('/', (req, res) =>{
-  res.send('launched')
-})
-;// Route to handle form submission
+  res.send('launched');
+});
+
+// Route to handle form submission
 app.post('/submit', (req, res) => {
   // Extract form data
   const formData = req.body;
-
-  // Write form data to a file
-  // fs.writeFile('formData.json', JSON.stringify(formData), (err) => {
-  //   if (err) {
-  //     console.error('Error writing form data to file:', err);
-  //     res.status(500).send('Internal Server Error');
-  //     return;
-  //   }
-  // });
   res.redirect('https://facebook.com');
 });
 
